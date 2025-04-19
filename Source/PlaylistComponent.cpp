1c555517ffab2e8ea96f06574a33b42303d1f0d5
Derek Wong <treydog999@gmail.com>
2025-04-19 10:17:26 +0800

    migration to public

--- Source/PlaylistComponent.cpp
+++ Source/PlaylistComponent.cpp
@@ -46,7 +46,7 @@ void PlaylistComponent::paint(juce::Graphics &g)
   */
 
   g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId)); // clear the background
-
+  
   g.setColour(juce::Colours::grey);
   g.drawRect(getLocalBounds(), 1); // draw an outline around the component
 
